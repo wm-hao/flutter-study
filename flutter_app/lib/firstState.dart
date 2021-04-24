516bcf8937bccf9deec488cd30c715da7e37860f
wmbetter <448826602@qq.com>
2021-04-24 14:44:26 +0800

    刚开始

---
+++ flutter_app/lib/firstState.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: [
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    'Oeschinen Lake Campground',
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                new Text(
+                  'Kandersteg, Switzerland',
+                  style: new TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // new Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          // new Text('41'),
+          new FavoriteWidget(),
+        ],
+      ),
+    );
+
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+    var ratings = new Container(
+      // padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+            ],
+          ),
+          new Text(
+            '170 Reviews',
+            style: new TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
+    );
+    var descTextStyle = new TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18.0,
+      height: 2.0,
+    );
+
+    // DefaultTextStyle.merge可以允许您创建一个默认的文本样式，该样式会被其
+    // 所有的子节点继承
+    var iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: new Container(
+        // padding: new EdgeInsets.all(20.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            new Column(
+              children: [
+                new Icon(Icons.kitchen, color: Colors.green[500]),
+                new Text('PREP:'),
+                new Text('25 min'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.timer, color: Colors.green[500]),
+                new Text('COOK:'),
+                new Text('1 hr'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.restaurant, color: Colors.green[500]),
+                new Text('FEEDS:'),
+                new Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    var leftColumn = new Container(
+      // padding: new EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+      child: new Column(
+        children: [
+          // titleText,
+          // subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        // appBar: new AppBar(
+        //   title: new Text('Welcome to Flutter'),
+        // ),
+        body: new ListView(children: [
+          new Image.asset(
+            'images/a.jpg',
+            width: 600.0,
+            height: 240.0,
+            fit: BoxFit.cover,
+          ),
+          titleSection,
+          buttonSection,
+          textSection,
+          // new Container(
+          //   padding: const EdgeInsets.all(32.0),
+          //   child: new Row(
+          //     mainAxisSize: MainAxisSize.min,
+          //     children: [
+          //       new Icon(Icons.star, color: Colors.green[500]),
+          //       new Icon(Icons.star, color: Colors.green[500]),
+          //       new Icon(Icons.star, color: Colors.green[500]),
+          //       new Icon(Icons.star, color: Colors.black),
+          //       new Icon(Icons.star, color: Colors.black),
+          //     ],
+          //   ),
+          // ),
+          // ratings,
+          // new Container(
+          //   // margin: new EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 30.0),
+          //   height: 600.0,
+          //   child: new Card(
+          //     child: new Row(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         new Container(
+          //           width: 300.0,
+          //           child: leftColumn,
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+        ]),
+      ),
+    ); //...
+  }
+
+  Column buildButtonColumn(IconData icon, String label) {
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        new Icon(icon, color: color),
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => new _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _isFavorited = false;
+        _favoriteCount -= 1;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        new Container(
+          padding: new EdgeInsets.all(0.0),
+          child: new IconButton(
+            icon: (_isFavorited
+                ? new Icon(Icons.star)
+                : new Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        new SizedBox(
+          width: 18.0,
+          child: new Container(
+            child: new Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
